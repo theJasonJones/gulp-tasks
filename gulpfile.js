687f652442bd66985d6c9dc39cdd9f54c16bd196
Jason Jones <jkjmr6@gmail.com>
2014-08-28 14:38:03 -0500

    Added sass, browserSync, autoprefixer, jshint, rename and uglify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,65 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-var changed = require('gulp-changed');
-var minifycss = require('gulp-minify-css');
-var autoprefixer = require('gulp-autoprefixer');
-var notify = require('gulp-notify');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    header  = require('gulp-header'),
+    rename = require('gulp-rename'),
+    minifyCSS = require('gulp-minify-css'),
+    package = require('./package.json');
 
-gulp.task('imagemin', function() {
-  var imgSrc = 'Phone Numbers/*',
-      imgDst = 'Pictures/min/images/Phone Numbers';
- 
-  gulp.src(imgSrc)
-    .pipe(changed(imgDst))
-    .pipe(imagemin())
-    .pipe(gulp.dest(imgDst));
+
+var banner = [
+  '/*!\n' +
+  ' * <%= package.name %>\n' +
+  ' * <%= package.title %>\n' +
+  ' * <%= package.url %>\n' +
+  ' * @author <%= package.author %>\n' +
+  ' * @version <%= package.version %>\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+  ' */',
+  '\n'
+].join('');
+
+gulp.task('css', function () {
+    return gulp.src('src/scss/style.scss')
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('css', function(){
-	return gulp.src('css/main.css')
-		.pipe(autoprefixer('last 15 version'))
-		.pipe(minifycss())
-		.pipe(gulp.dest('css/min'))
-		.pipe(notify({message: 'Finished with CSS.'}));
-})
+gulp.task('js',function(){
+  gulp.src('src/js/scripts.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(uglify())
+    .pipe(header(banner, { package : package }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(browserSync.reload({stream:true, once: true}));
+});
 
-gulp.task('default', function(){
-	gulp.run('imagemin');
-	gulp.run('css');
-})
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: "app"
+        }
+    });
+});
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['css', 'js', 'browser-sync'], function () {
+    gulp.watch("src/scss/*/*.scss", ['css']);
+    gulp.watch("src/js/*.js", ['js']);
+    gulp.watch("app/*.html", ['bs-reload']);
+});
