ca555e996d41709cc6f58ac3240f531d41cd7b00
Jason Jones <jkjmr6@gmail.com>
2015-04-28 10:51:04 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,170 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    autoprefixer = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglify'),
-    jshint = require('gulp-jshint'),
-    header  = require('gulp-header'),
-    rename = require('gulp-rename'),
-    minifyCSS = require('gulp-minify-css'),
-    package = require('./package.json'),
-    imagemin = require('gulp-imagemin'),
-    pngcrush = require('imagemin-pngcrush');
+var fs = require('fs');
+var path = require('path');
 
+var gulp = require('gulp');
 
-var banner = [
-  '/*!\n' +
-  ' * <%= package.name %>\n' +
-  ' * <%= package.title %>\n' +
-  ' * <%= package.url %>\n' +
-  ' * @author <%= package.author %>\n' +
-  ' * @version <%= package.version %>\n' +
-  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
-  ' */',
-  '\n'
-].join('');
+// Load all gulp plugins automatically
+// and attach them to the `plugins` object
+var plugins = require('gulp-load-plugins')();
 
-//Clean up CSS
-gulp.task('css', function () {
-    return gulp.src('src/scss/style.scss')
-    .pipe(sass({errLogToConsole: true}))
-    .pipe(autoprefixer('last 4 version'))
-    .pipe(gulp.dest('app/assets/css'))
-    .pipe(minifyCSS())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(header(banner, { package : package }))
-    .pipe(gulp.dest('app/assets/css'))
-    .pipe(browserSync.reload({stream:true}));
+// Temporary solution until gulp 4
+// https://github.com/gulpjs/gulp/issues/355
+var runSequence = require('run-sequence');
+
+var pkg = require('./package.json');
+var dirs = pkg['h5bp-configs'].directories;
+
+// ---------------------------------------------------------------------
+// | Helper tasks                                                      |
+// ---------------------------------------------------------------------
+
+gulp.task('archive:create_archive_dir', function () {
+    fs.mkdirSync(path.resolve(dirs.archive), '0755');
 });
 
-//Lint Javascript
-gulp.task('js',function(){
-  gulp.src('src/js/scripts.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(header(banner, { package : package }))
-    .pipe(gulp.dest('app/assets/js'))
-    .pipe(uglify())
-    .pipe(header(banner, { package : package }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('app/assets/js'))
-    .pipe(browserSync.reload({stream:true, once: true}));
-});
+gulp.task('archive:zip', function (done) {
 
-//Optimize images
-gulp.task('img', function () {
-    return gulp.src('src/images/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngcrush()]
-        }))
-        .pipe(gulp.dest('dist'));
-});
-
-
-//Watch for changes and reload the browser
-gulp.task('browser-sync', function() {
-    browserSync.init(null, {
-        server: {
-            baseDir: "app"
-        }
+    var archiveName = path.resolve(dirs.archive, pkg.name + '_v' + pkg.version + '.zip');
+    var archiver = require('archiver')('zip');
+    var files = require('glob').sync('**/*.*', {
+        'cwd': dirs.dist,
+        'dot': true // include hidden files
     });
-});
-gulp.task('bs-reload', function () {
-    browserSync.reload();
+    var output = fs.createWriteStream(archiveName);
+
+    archiver.on('error', function (error) {
+        done();
+        throw error;
+    });
+
+    output.on('close', done);
+
+    files.forEach(function (file) {
+
+        var filePath = path.resolve(dirs.dist, file);
+
+        // `archiver.bulk` does not maintain the file
+        // permissions, so we need to add files individually
+        archiver.append(fs.createReadStream(filePath), {
+            'name': file,
+            'mode': fs.statSync(filePath)
+        });
+
+    });
+
+    archiver.pipe(output);
+    archiver.finalize();
+
 });
 
-// Run PageSpeed Insights
-// Update `url` below to the public URL for your site
-gulp.task('pagespeed', pagespeed.bind(null, {
-  // By default, we use the PageSpeed Insights
-  // free (no API key) tier. You can use a Google
-  // Developer API key if you have one. See
-  // http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
-  url: 'https://example.com',
-  strategy: 'mobile'
-}));
-
-gulp.task('default', ['css', 'js', 'browser-sync'], function () {
-    gulp.watch("src/scss/*/*.scss", ['css']);
-    gulp.watch("src/js/*.js", ['js']);
-    gulp.watch("app/*.html", ['bs-reload']);
+gulp.task('clean', function (done) {
+    require('del')([
+        dirs.archive,
+        dirs.dist
+    ], done);
 });
+
+gulp.task('copy', [
+    'copy:.htaccess',
+    'copy:index.html',
+    'copy:jquery',
+    'copy:license',
+    'copy:main.css',
+    'copy:misc',
+    'copy:normalize'
+]);
+
+gulp.task('copy:.htaccess', function () {
+    return gulp.src('node_modules/apache-server-configs/dist/.htaccess')
+               .pipe(plugins.replace(/# ErrorDocument/g, 'ErrorDocument'))
+               .pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('copy:index.html', function () {
+    return gulp.src(dirs.src + '/index.html')
+               .pipe(plugins.replace(/{{JQUERY_VERSION}}/g, pkg.devDependencies.jquery))
+               .pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('copy:jquery', function () {
+    return gulp.src(['node_modules/jquery/dist/jquery.min.js'])
+               .pipe(plugins.rename('jquery-' + pkg.devDependencies.jquery + '.min.js'))
+               .pipe(gulp.dest(dirs.dist + '/js/vendor'));
+});
+
+gulp.task('copy:license', function () {
+    return gulp.src('LICENSE.txt')
+               .pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('copy:main.css', function () {
+
+    var banner = '/*! HTML5 Boilerplate v' + pkg.version +
+                    ' | ' + pkg.license.type + ' License' +
+                    ' | ' + pkg.homepage + ' */\n\n';
+
+    return gulp.src(dirs.src + '/css/main.css')
+               .pipe(plugins.header(banner))
+               .pipe(plugins.autoprefixer({
+                   browsers: ['last 2 versions', 'ie >= 8', '> 1%'],
+                   cascade: false
+               }))
+               .pipe(gulp.dest(dirs.dist + '/css'));
+});
+
+gulp.task('copy:misc', function () {
+    return gulp.src([
+
+        // Copy all files
+        dirs.src + '/**/*',
+
+        // Exclude the following files
+        // (other tasks will handle the copying of these files)
+        '!' + dirs.src + '/css/main.css',
+        '!' + dirs.src + '/index.html'
+
+    ], {
+
+        // Include hidden files by default
+        dot: true
+
+    }).pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('copy:normalize', function () {
+    return gulp.src('node_modules/normalize.css/normalize.css')
+               .pipe(gulp.dest(dirs.dist + '/css'));
+});
+
+gulp.task('lint:js', function () {
+    return gulp.src([
+        'gulpfile.js',
+        dirs.src + '/js/*.js',
+        dirs.test + '/*.js'
+    ]).pipe(plugins.jscs())
+      .pipe(plugins.jshint())
+      .pipe(plugins.jshint.reporter('jshint-stylish'))
+      .pipe(plugins.jshint.reporter('fail'));
+});
+
+
+// ---------------------------------------------------------------------
+// | Main tasks                                                        |
+// ---------------------------------------------------------------------
+
+gulp.task('archive', function (done) {
+    runSequence(
+        'build',
+        'archive:create_archive_dir',
+        'archive:zip',
+    done);
+});
+
+gulp.task('build', function (done) {
+    runSequence(
+        ['clean', 'lint:js'],
+        'copy',
+    done);
+});
+
+gulp.task('default', ['build']);
